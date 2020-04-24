540b00ace15cd254745e9c87e14c356aa00f1b0e
niko <niko.tastula@gmail.com>
2020-04-24 14:27:31 +0300

    readme skeleton

--- src/app/core/util/util.ts
+++ src/app/core/util/util.ts
@@ -1,15 +1,14 @@
-// helper for finding ONE possible route color within arrays of line colors
+// helper for finding same string from all arrays given as a parameter
+// could also return an array of string weighing by cost or similar
+// but since all "cost" the same we don't care if we take green or yellow line as long as it takes us where we want to
 export const findSimilarValuesInArrays = arrayOfArrays => {
   let found = '';
-  console.log(arrayOfArrays);
   for (const arr of arrayOfArrays) {
     for (const color of arr) {
-      // if found in all, does not matter which color - lines all "cost" the same amount
       if (arrayOfArrays.filter(a => a.includes(color)).length === arrayOfArrays.length) {
         found = color;
       }
     }
   }
-  console.log(found);
   return found;
 };
